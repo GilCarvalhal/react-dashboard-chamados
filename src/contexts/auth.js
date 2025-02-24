68e867f4f61471bb561bebf96a596ad32ac17cfc
gilberto.matos <gilberto.matos@salvador.ba.gov.br>
2025-02-24 10:53:55 -0300

    Cadastrando usuário

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,9 +1,13 @@
 import { useState, useEffect, createContext } from "react";
+import { auth, db } from "../services/firebaseConnection";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-  const [user, serUser] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
 
   function signIn(email, password) {
     console.log(email);
@@ -11,12 +15,50 @@ function AuthProvider({ children }) {
     alert("Logado com sucesso!");
   }
 
+  // Cadastrar um novo user
+  async function signUp(email, password, name) {
+    setLoadingAuth(true);
+
+    if (password.length < 6) {
+      alert("A senha deve ter pelo menos 6 caracteres.");
+      setLoadingAuth(false);
+      return;
+    }
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+
+        await setDoc(doc(db, "users", uid), {
+          nome: name,
+          avatarUrl: null,
+        }).then(() => {
+          let data = {
+            uid: uid,
+            nome: name,
+            email: value.user.email,
+            avatarUrl: null,
+          };
+
+          setUser(data);
+
+          setLoadingAuth(false);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoadingAuth(false);
+      });
+  }
+
   return (
     <AuthContext.Provider
       value={{
-        signed: !!user, // false
+        signed: !!user, // !! faz com que o valor seja sempre booleano.
         user,
         signIn,
+        signUp,
+        loadingAuth,
       }}
     >
       {children}
