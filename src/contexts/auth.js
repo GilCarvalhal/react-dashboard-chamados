c524ae9af673bc8375d7619cb45abc42cc2f82e4
gilberto.matos <gilberto.matos@salvador.ba.gov.br>
2025-02-25 10:12:21 -0300

    Criando páginas privadas

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,8 +1,9 @@
 import { useState, useEffect, createContext } from "react";
-import { auth, db, storage } from "../services/firebaseConnection";
+import { auth, db } from "../services/firebaseConnection";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -14,8 +15,22 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadUser() {
+      const storageUser = localStorage.getItem("@tickets");
+
+      if (storageUser) {
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+      setLoading(false);
+    }
+    loadUser();
+  }, []);
 
   async function signIn(email, password) {
     setLoadingAuth(true);
@@ -98,6 +113,12 @@ function AuthProvider({ children }) {
     localStorage.setItem("@tickets", JSON.stringify(data));
   }
 
+  async function logout() {
+    await signOut(auth);
+    localStorage.removeItem("@tickets");
+    setUser(null);
+  }
+
   return (
     <AuthContext.Provider
       value={{
@@ -105,7 +126,9 @@ function AuthProvider({ children }) {
         user,
         signIn,
         signUp,
+        logout,
         loadingAuth,
+        loading,
       }}
     >
       {children}
